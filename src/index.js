933a9179faac2f377718b7213808526dd5960e3f
vero <vero.dm@yandex.ru>
2024-11-29 00:23:03 +0300

    добавлены вып.окна

--- src/index.js
+++ src/index.js
@@ -75,3 +75,14 @@ var swiper = new Swiper(".offerSwiper", {
     }
     
   });
+
+
+  //Dropdown
+
+  function openModal (){
+    document.getElementById("dropdown").style.display = "block";
+  }
+
+  function closeModal (){
+    document.getElementById("dropdown").style.top = "-400px";
+  }
